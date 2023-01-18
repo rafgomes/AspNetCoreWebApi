d2f1a13a92445060485e95f63ae2005f877f3b30
rafgomes <rafgomes@gmail.com>
2023-01-17 23:08:08 -0300

    Heroi Projet

---
+++ EFCore.Repo/Migrations/20230118020316_initial.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore.Repo.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Batalhas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DtInicio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DtFim = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Batalhas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Herois",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Herois", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Armas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HeroiId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Armas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Armas_Herois_HeroiId",
+                        column: x => x.HeroiId,
+                        principalTable: "Herois",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HeroisBatalhas",
+                columns: table => new
+                {
+                    HeroiId = table.Column<int>(type: "int", nullable: false),
+                    BatalhaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HeroisBatalhas", x => new { x.BatalhaId, x.HeroiId });
+                    table.ForeignKey(
+                        name: "FK_HeroisBatalhas_Batalhas_BatalhaId",
+                        column: x => x.BatalhaId,
+                        principalTable: "Batalhas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HeroisBatalhas_Herois_HeroiId",
+                        column: x => x.HeroiId,
+                        principalTable: "Herois",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IdentidadesSecretas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeReal = table.Column<int>(type: "int", nullable: false),
+                    HeroiId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdentidadesSecretas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IdentidadesSecretas_Herois_HeroiId",
+                        column: x => x.HeroiId,
+                        principalTable: "Herois",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Armas_HeroiId",
+                table: "Armas",
+                column: "HeroiId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HeroisBatalhas_HeroiId",
+                table: "HeroisBatalhas",
+                column: "HeroiId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdentidadesSecretas_HeroiId",
+                table: "IdentidadesSecretas",
+                column: "HeroiId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Armas");
+
+            migrationBuilder.DropTable(
+                name: "HeroisBatalhas");
+
+            migrationBuilder.DropTable(
+                name: "IdentidadesSecretas");
+
+            migrationBuilder.DropTable(
+                name: "Batalhas");
+
+            migrationBuilder.DropTable(
+                name: "Herois");
+        }
+    }
+}
